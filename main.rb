5f27a1c7a968dffb43d46da0731983ebd1d8f0ca
kuro <kuro@KURO-no-MacBook-Pro.local>
2016-03-03 19:32:24 +0900

    FN Prac4

--- main.rb
+++ main.rb
@@ -1,5 +1,21 @@
 require 'sinatra'
+require 'sinatra/reloader'
 
-get '/' do
-  "hello world"
+=begin
+get '/hello/:name' do
+  "hello #{params[:name]}"
+end
+
+get '/hello/:name' do |n|
+  "hello #{n}"
+end
+
+get '/hello/:fname/:lname' do |f,l|
+  "hello #{f} #{l}"
+end
+
+=end
+
+get '/hello/:fname/?:lname?' do |f,l|
+  "hello #{f} #{l}"
 end
