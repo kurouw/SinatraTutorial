dd02beefe4385955c14d8c9146dfb63fd332a647
kurouw <github0220@gmail.com>
2016-03-05 01:13:50 +0900

    FN Prac12

--- main2.rb
+++ main2.rb
@@ -1,6 +1,16 @@
-reauire 'sinatra'
+require 'sinatra'
 require 'sinatra/reloader'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+  "adapter" => "sqlite3",
+  "database" => "./bbs.db"
+)
+
+class Comment < ActiveRecord::Base
+end
 
 get '/' do
+  @comments = Comment.order("id desc").all
   erb :index
 end
